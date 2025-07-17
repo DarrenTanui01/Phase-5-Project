c4b7a73888cb1f81b23f5f89a080a040840c9a28
DarrenTanui01 <167513705+DarrenTanui01@users.noreply.github.com>
2025-07-17 20:27:36 +0300

    Added nested fields to some schemas for one-to-many relationship embedding

--- backend/schemas.py
+++ backend/schemas.py
@@ -11,11 +11,13 @@ class SupplierSchema(Schema):
     id = fields.Int(dump_only=True)
     name = fields.Str(required=True)
     contact_info = fields.Str()
+    products = fields.Nested('ProductSchema', many=True, dump_only=True)  
 
 class CustomerSchema(Schema):
     id = fields.Int(dump_only=True)
     name = fields.Str(required=True)
     contact_info = fields.Str()
+    orders = fields.Nested('OrderSchema', many=True, dump_only=True) 
 
 class ProductSchema(Schema):
     id = fields.Int(dump_only=True)
@@ -49,13 +51,6 @@ class PermissionSchema(Schema):
     name = fields.Str(required=True)
     role_id = fields.Int()
 
-class BankAccountSchema(Schema):
-    id = fields.Int(dump_only=True)
-    account_name = fields.Str(required=True)
-    account_number = fields.Str(required=True)
-    bank_name = fields.Str(required=True)
-    balance = fields.Float()
-
 class BankTransactionSchema(Schema):
     id = fields.Int(dump_only=True)
     bank_account_id = fields.Int(required=True)
@@ -63,6 +58,14 @@ class BankTransactionSchema(Schema):
     transaction_type = fields.Str(required=True)
     date = fields.DateTime()
     description = fields.Str()
+
+class BankAccountSchema(Schema):
+    id = fields.Int(dump_only=True)
+    account_name = fields.Str(required=True)
+    account_number = fields.Str(required=True)
+    bank_name = fields.Str(required=True)
+    balance = fields.Float()
+    transactions = fields.Nested('BankTransactionSchema', many=True, dump_only=True)  
 
 class PostingSchema(Schema):
     id = fields.Int(dump_only=True)
